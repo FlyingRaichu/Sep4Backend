e2fc8a76d7fe0ece24beed4b49cd872ce6aea63a
Lyubomir Veselinov <lyubomir.veselinov@gmail.com>
2024-04-25 10:14:16 +0200

    Pushed connection string to its own settings file, then gitignored it.

--- DatabaseInterfacing/DatabaseUtils.cs
+++ DatabaseInterfacing/DatabaseUtils.cs
@@ -3,27 +3,36 @@
 using DatabaseInterfacing.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 namespace DatabaseInterfacing;
 
-public class DatabaseUtils : IDesignTimeDbContextFactory<PlantDbContext>
+public abstract class DatabaseUtils : IDesignTimeDbContextFactory<PlantDbContext>
 {
-    private const string ConnectionString = "Host=lucky.db.elephantsql.com;Port=5432;Database=odvlulua;Username=odvlulua;Password=oBlgMTohINy3iW30-RxXfKY0GyrtS1c2;SSL Mode=Require;Trust Server Certificate=true;";
+    private static readonly IConfiguration configuration = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        .Build();
 
     public static DbContextOptions<PlantDbContext> BuildConnectionOptions()
     {
         var optionsBuilder = new DbContextOptionsBuilder<PlantDbContext>();
-        optionsBuilder.UseNpgsql(ConnectionString);
+        optionsBuilder.UseNpgsql(GetConnectionString());
 
         return optionsBuilder.Options;
     }
 
-    
+    private static string GetConnectionString()
+    {
+        return configuration.GetConnectionString("DefaultConnection") ??
+               "Host=lucky.db.elephantsql.com;Port=5432;Database=odvlulua;Username=odvlulua;Password=oBlgMTohINy3iW30-RxXfKY0GyrtS1c2;SSL Mode=Require;Trust Server Certificate=true;";
+    }
+
+
     //This is for Migrations, no touchie
     public PlantDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<PlantDbContext>();
-        optionsBuilder.UseNpgsql(ConnectionString);
+        optionsBuilder.UseNpgsql(GetConnectionString());
 
         return new PlantDbContext(optionsBuilder.Options);
     }
