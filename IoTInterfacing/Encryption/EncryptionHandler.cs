3b2092e607315332d0457a4ef75f9179b84762a5
Lyubomir Veselinov <lyubomir.veselinov@gmail.com>
2024-05-21 17:48:04 +0200

    Encryption added, public key added to message, removed unnecessary private key instance

--- IoTInterfacing/Encryption/EncryptionHandler.cs
+++ IoTInterfacing/Encryption/EncryptionHandler.cs
@@ -1,35 +1,29 @@
 ﻿using System.Security.Cryptography;
+using System.Text;
 
 namespace IoTInterfacing.Encryption;
 
 public class EncryptionHandler : IEncryptionHandler
 {
-    public byte[] PrivateKey { get; private set; }
-    public byte[] PublicKey { get; private set; }
-    private byte[] _aesKey;
+    private byte[] PublicKey { get;} = GenerateEccKeys();
+    private byte[] _aesKey = new byte[32];
 
-    public EncryptionHandler()
-    {
-        (PublicKey, PrivateKey) = GenerateEccKeys();
-        _aesKey = new byte[32];
-    }
     // Method for generating ECC keys
-    private static (byte[] PublicKey, byte[] PrivateKey) GenerateEccKeys()
+    private static byte[] GenerateEccKeys()
     {
-        using var ecdh = ECDiffieHellman.Create();
-        var publicKey = ecdh.ExportSubjectPublicKeyInfo();
-        var privateKey = ecdh.ExportECPrivateKey();
+        using var eccGenerator = ECDiffieHellman.Create();
+        var publicKey = eccGenerator.ExportSubjectPublicKeyInfo();
 
-        return (publicKey, privateKey);
+        return (publicKey);
     }
 
     // Method for deriving shared secret and AES key
     public void DeriveSharedSecret(byte[] theirPublicKey)
     {
-        using var ecdh = ECDiffieHellman.Create();
-        ecdh.ImportSubjectPublicKeyInfo(theirPublicKey, out _);
+        using var eccGenerator = ECDiffieHellman.Create();
+        eccGenerator.ImportSubjectPublicKeyInfo(theirPublicKey, out _);
 
-        var sharedSecret = ecdh.DeriveKeyMaterial(ecdh.PublicKey);
+        var sharedSecret = eccGenerator.DeriveKeyMaterial(eccGenerator.PublicKey);
         _aesKey = sharedSecret;
     }
 
@@ -41,28 +35,45 @@ public class EncryptionHandler : IEncryptionHandler
         aes.IV = iv;
 
         using var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
-        using var ms = new MemoryStream(cipherText);
-        using var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read);
-        using var sr = new StreamReader(cs);
+        using var memoryStream = new MemoryStream(cipherText);
+        using var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+        using var streamReader = new StreamReader(cryptoStream);
 
-        return sr.ReadToEnd();
+        return streamReader.ReadToEnd();
     }
 
     public byte[] EncryptAes(string plainText, out byte[] iv)
     {
         using var aes = Aes.Create();
-        aes.Key = _aesKey;
         aes.GenerateIV();
         iv = aes.IV;
 
-        using var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
-        using var ms = new MemoryStream();
-        using var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write);
-        using var sw = new StreamWriter(cs);
+        // Combine the sender's public key with the plainText
+        var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+        var message = new byte[4 + PublicKey.Length + plainTextBytes.Length];
 
-        sw.Write(plainText);
-        sw.Close();
+        // Write the length of the public key bytes as a 4-byte prefix
+        var lengthBytes = BitConverter.GetBytes(PublicKey.Length);
+        Buffer.BlockCopy(lengthBytes, 0, message, 0, lengthBytes.Length);
 
-        return ms.ToArray();
+        // Write the public key bytes
+        Buffer.BlockCopy(PublicKey, 0, message, 4, PublicKey.Length);
+
+        // // Use a delimiter to separate the public key from the encrypted text //TODO this is possibly an alternative, having a sign be the breakpoint if encoding the public key length isn't enough
+        // const byte delimiter = 0x1F;
+        // message[4 + publicKeyBytes.Length] = delimiter;
+
+        // Write the encrypted text bytes
+        Buffer.BlockCopy(plainTextBytes, 0, message, 4 + PublicKey.Length + 1, plainTextBytes.Length);
+
+        // Encrypt the combined message
+        using var encryptor = aes.CreateEncryptor(_aesKey, aes.IV);
+        using var memoryStream = new MemoryStream();
+        using var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+        cryptoStream.Write(message, 0, message.Length);
+        cryptoStream.FlushFinalBlock();
+
+        return memoryStream.ToArray();
     }
+
 }
